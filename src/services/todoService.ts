f87ceb62f606d0b30fb8742103b98aebba77f920
femmer <jfemmer.business@gmail.com>
2020-06-01 15:28:20 -0500

    Add Post Request to form

--- src/services/todoService.ts
+++ src/services/todoService.ts
@@ -18,18 +18,38 @@ export const getTodos = async () => {
 
 };
 
-export const getTodoById = (id: number) => {
-
+export const postTodo = async (todo: Todo) => {
+    try{
+        let response = await axios.post(base_url, todo);
+        console.log("Response", response);
+        console.log("Data", response.data);
+        return response.data;
+    }
+    catch (error) {
+        return error;
+    }
 }
 
-export const postTodo = (todo: Todo) => {
+export const updateTodoById = async (todo: Todo) => {
+    try{
+        let response = await axios.post(`${base_url}/${todo.id}`, todo);
+        console.log("Response", response);
+        console.log("Data", response.data);
+        return response.data;
+    }
+    catch (error) {
+        return error;
 
+    }
 }
 
-export const updateTodoById = (todo: Todo) => {
+export const deleteTodoById = async (id: number) => {
+    try{
+        let response = await axios.post(`${base_url}/${id}`);
+        console.log("Response", response);
+    }
+    catch (error) {
+        return error;
 
-}
-
-export const deleteTodoById =(id: number) => {
-
+    }
 }
